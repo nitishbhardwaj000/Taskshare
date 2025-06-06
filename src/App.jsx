deaba9c72e74bc5f27a812b8d6109a3ffbb00080
nitishbhardwaj1279 <bhardwajnitish1270@gmail.com>
2025-06-06 19:37:45 -0400

    Addes useStae to show multiple tasks

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,40 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TaskCard from './components/TaskCard'
 
 function App() {
   
+  const [tasks,setTasks]=useState([
+    {
+      id:"1",
+      title:"Task1",
+      description:"leanr react basics",
+      status:"pending",
+    },{
+      id:"2",
+      title:"Task2",
+      description:"pick grocery from store",
+      status:"pending",
+    },{
+      id:"3",
+      title:"Task3",
+      description:"go to gym",
+      status:"completed",
+    }
+  ])
+ 
 
   return (
     <div style={{textAlign:"center",margintop:"30rem"}}>
+     
       <h1>Hello TaskShare</h1>
-      <p>A microtask collaboration Platofrm</p>
-
+     <div style={{display:"flex", justifyContent:"center",flexWrap:"wrap"}}>
+        {tasks.map((task)=>{
+          return <TaskCard key={task.id} description={task.description} title={task.title} status={task.status}/>
+        })}
+    </div>
+    
     </div>
   )
 }
