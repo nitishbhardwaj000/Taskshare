13ff599deded8f151bbec098e7b9f0904159497e
nitishbhardwaj1279 <bhardwajnitish1270@gmail.com>
2025-06-06 18:43:53 -0400

    initail commit Tasksharebasic Setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div style={{textAlign:"center",margintop:"30rem"}}>
+      <h1>Hello TaskShare</h1>
+      <p>A microtask collaboration Platofrm</p>
+
+    </div>
+  )
+}
+
+export default App
